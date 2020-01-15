40986d4886314fa92d70148ecf268faf8400086d
Tom Oliver <tom.oliver@bizreach.co.jp>
2020-01-15 11:32:03 +0900

    Made bubble sort more efficient

--- dart/sort/all/lib/bubble.dart
+++ dart/sort/all/lib/bubble.dart
@@ -2,16 +2,15 @@
 bubbleSort(List<num> paramList){
   List<num> list = []..addAll(paramList);
   var changed = true;
-  while(changed){
-    changed = false;
-    for (var i = 0; i < list.length-1; i++){
-      if (list[i] > list[i+1]){
-        var temp = list[i];
-        list[i] = list[i+1];
-        list[i+1] = temp;
-        changed = true;
+    for (var i = list.length-1; i > 0 && changed; i--){
+      for (var j = 0; j < i; j++){
+        if (list[j] > list[j+1]){
+          var temp = list[j];
+          list[j] = list[j+1];
+          list[j+1] = temp;
+          changed = true;
+        }
       }
     }
-  }
   return list;
 }
